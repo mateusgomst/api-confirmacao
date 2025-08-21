ee710f91525937d22c4a0dade79aca86ef639336
mateus.tcastro <mateus.tcastro@goias.go.gov>
2025-08-21 17:05:12 -0300

    refactor: update PacienteController to use PacienteServiceInterface and implement listarPacientes method

--- src/main/java/com/aba_mais/api_confirmacao/controller/PacienteController.java
+++ src/main/java/com/aba_mais/api_confirmacao/controller/PacienteController.java
@@ -1,20 +1,25 @@
 package com.aba_mais.api_confirmacao.controller;
 
-import com.aba_mais.api_confirmacao.dto.CreatePacienteRequestDto;
-import com.aba_mais.api_confirmacao.entity.Paciente;
-import com.aba_mais.api_confirmacao.service.PacienteService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.aba_mais.api_confirmacao.dto.CreatePacienteRequestDto;
+import com.aba_mais.api_confirmacao.entity.Paciente;
+import com.aba_mais.api_confirmacao.interfaces.PacienteServiceInterface;
 
 @RestController
 @RequestMapping("/api/pacientes")
 public class PacienteController {
 
     @Autowired
-    private PacienteService pacienteService;
+    private PacienteServiceInterface pacienteService;
 
     @PostMapping
     public ResponseEntity<Paciente> cadastrarPaciente(@RequestBody CreatePacienteRequestDto paciente) {
