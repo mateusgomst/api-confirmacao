600f97f5c88af6b4a85292484695424fdc4e3961
mateusgomst <mateusgomes9119@gmail.com>
2025-08-21 13:01:38 -0300

    add: implement listarPacientes method in PacienteController and PacienteService for patient retrieval

--- src/main/java/com/aba_mais/api_confirmacao/service/PacienteService.java
+++ src/main/java/com/aba_mais/api_confirmacao/service/PacienteService.java
@@ -3,14 +3,27 @@ package com.aba_mais.api_confirmacao.service;
 import com.aba_mais.api_confirmacao.dto.CreatePacienteRequestDto;
 import com.aba_mais.api_confirmacao.entity.Paciente;
 import com.aba_mais.api_confirmacao.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PacienteService {
+
+    @Autowired
     private PacienteRepository pacienteRepository;
-    public Paciente cadastrarPaciente(CreatePacienteRequestDto pacienteDto){
-        Paciente paciente = new Paciente(pacienteDto.getNome(), pacienteDto.getEmail(), pacienteDto.getTelefone());
-        pacienteRepository.save(paciente);
-        return paciente;
+
+    public Paciente cadastrarPaciente(CreatePacienteRequestDto pacienteDto) {
+        Paciente paciente = new Paciente(
+                pacienteDto.getNome(),
+                pacienteDto.getEmail(),
+                pacienteDto.getTelefone()
+        );
+        return pacienteRepository.save(paciente);
+    }
+
+    public List<Paciente> listarPacientes() {
+        return pacienteRepository.findAll();
     }
 }
