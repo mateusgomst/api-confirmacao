3aaf16cd7acf72a72d91f372a51f5d7702da198c
mateusgomst <mateusgomes9119@gmail.com>
2025-08-21 11:46:07 -0300

    add: implement CreatePacienteRequestDto, PacienteController, PacienteService, and PacienteRepository for patient registration

--- src/main/java/com/aba_mais/api_confirmacao/controller/PacienteController.java
+++ src/main/java/com/aba_mais/api_confirmacao/controller/PacienteController.java
@@ -1,7 +1,11 @@
 package com.aba_mais.api_confirmacao.controller;
 
+import com.aba_mais.api_confirmacao.dto.CreatePacienteRequestDto;
+import com.aba_mais.api_confirmacao.entity.Paciente;
+import com.aba_mais.api_confirmacao.service.PacienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PacienteController{
 
+    private PacienteService pacienteService;
     @PostMapping()
-    public ResponseEntity<String> cadastrarPaciente(){
-        return ResponseEntity.ok("Paciente cadastrado com sucesso!");
+    public ResponseEntity<Paciente> cadastrarPaciente(@RequestBody CreatePacienteRequestDto paciente) {
+        return ResponseEntity.ok(pacienteService.cadastrarPaciente(paciente));
     }
 }
--- src/main/java/com/aba_mais/api_confirmacao/service/PacienteService.java
+++ src/main/java/com/aba_mais/api_confirmacao/service/PacienteService.java
@@ -1,11 +1,16 @@
 package com.aba_mais.api_confirmacao.service;
 
+import com.aba_mais.api_confirmacao.dto.CreatePacienteRequestDto;
 import com.aba_mais.api_confirmacao.entity.Paciente;
+import com.aba_mais.api_confirmacao.repository.PacienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PacienteService {
-    public Paciente cadastrarPaciente(Paciente paciente){
+    private PacienteRepository pacienteRepository;
+    public Paciente cadastrarPaciente(CreatePacienteRequestDto pacienteDto){
+        Paciente paciente = new Paciente(pacienteDto.getNome(), pacienteDto.getEmail(), pacienteDto.getTelefone());
+        pacienteRepository.save(paciente);
         return paciente;
     }
 }
