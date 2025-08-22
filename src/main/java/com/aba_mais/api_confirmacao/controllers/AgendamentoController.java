175273d7ce376c1615481b7e64717f27a0493d3e
Mateus Gomes <mateusgomes9119@gmail.com>
2025-08-22 15:47:40 -0300

    Implement scheduling service with CRUD operations and validations in AgendamentoService, Controller, and Repository

--- src/main/java/com/aba_mais/api_confirmacao/controllers/AgendamentoController.java
+++ src/main/java/com/aba_mais/api_confirmacao/controllers/AgendamentoController.java
@@ -1,10 +1,16 @@
 package com.aba_mais.api_confirmacao.controllers;
 
+import com.aba_mais.api_confirmacao.dtos.AgendamentoResponseDto;
+import com.aba_mais.api_confirmacao.dtos.CriarAgendamentoRequestDto;
+import com.aba_mais.api_confirmacao.entities.Agendamento;
 import com.aba_mais.api_confirmacao.interfaces.AgendamentoServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/agendamentos")
@@ -14,8 +20,26 @@ public class AgendamentoController {
     private AgendamentoServiceInterface agendamentoService;
 
     @PostMapping
-    public String agendarConsulta() {
-        // Implementar lógica de agendamento de consulta
-        return "Consulta agendada com sucesso!";
+    public ResponseEntity<Agendamento> agendarConsulta(@Validated @RequestBody CriarAgendamentoRequestDto agendamentoDto) {
+        Agendamento agendamento = agendamentoService.criarAgendamento(
+                agendamentoDto.getPacienteId(),
+                agendamentoDto.getDataHora()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(agendamento);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AgendamentoResponseDto>> listarAgendamentos() {
+        return ResponseEntity.ok(agendamentoService.listarAgendamentos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AgendamentoResponseDto> buscarAgendamentoPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(agendamentoService.buscarAgendamentoPorId(id));
+    }
+
+    @GetMapping("/paciente/{id}")
+    public ResponseEntity<List<AgendamentoResponseDto>> buscarAgendamentosPorPacienteId(@PathVariable Long id) {
+        return ResponseEntity.ok(agendamentoService.listarAgendamentosPorPacienteId(id));
     }
 }
